b1152c318b1c3524002adf285c672c8741850f07
Aleksandar Diklic <rastasheep3@gmail.com>
2018-09-29 20:49:11 +0200

    Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html.ejs'),
+    }),
+  ]
+};
