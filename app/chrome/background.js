bbb8156cabd40ae5d33b6f69a12424c61cd3b8f9
Aleksandar Diklic <rastasheep3@gmail.com>
2018-10-23 23:23:13 +0200

    Be smart when opening extension tab

--- app/chrome/background.js
+++ app/chrome/background.js
@@ -1,15 +1,24 @@
 chrome.browserAction.onClicked.addListener(() => {
   chrome.tabs.query({ url: chrome.extension.getURL('*') }, tabs => {
+    const appTab = tabs[0];
     if (tabs.length) {
       chrome.tabs.highlight({
-        windowId: tabs[0].windowId,
-        tabs: tabs[0].index,
+        windowId: appTab.windowId,
+        tabs: appTab.index,
       });
+      chrome.windows.update(appTab.windowId, { focused: true });
       return;
     }
 
-    chrome.tabs.create({
-      url: chrome.extension.getURL('index.html'),
+    chrome.tabs.query({ lastFocusedWindow: true, active: true }, activeTabs => {
+      const currentTab = activeTabs[0];
+      const url = chrome.extension.getURL('index.html');
+      if (currentTab.url === 'chrome://newtab/') {
+        chrome.tabs.update(currentTab.id, { url });
+        return;
+      }
+
+      chrome.tabs.create({ url });
     });
   });
 });
